9b16d3f43119c8028b3d899e429a907904a5ef5b
Marcuss17 <eduardomm171@gmail.com>
2019-09-10 12:20:34 -0500

    Modifying FIFO.cpp

--- FIFO.cpp
+++ FIFO.cpp
@@ -1,5 +1,14 @@
-//
-// Created by Eduardo Medina on 10/09/2019.
-//
-
 #include "FIFO.h"
+#include <iostream>
+
+FIFO::FIFO() {
+    n1 = nullptr;
+}
+
+void FIFO::addNode(int value) {
+    Node* new_node = new Node();
+    new_node->value = value;
+    new_node->next = n1;
+    n1 = new_node;
+    std::cout<<"Node of with the value of " << value << " has been added to the FIFO structure." << std::endl;
+}
